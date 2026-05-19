a6e58588deec053c7413ca1198c66fc27d736e8e
nadcha <nnadine.chargui@gmail.com>
2026-05-19 16:46:35 -0400

    Update base URLs for IoT devices

--- src/config/iotDevices.ts
+++ src/config/iotDevices.ts
@@ -1,10 +1,10 @@
 export const IOT_DEVICES = {
   trainPi: {
     deviceId: "rasp50",
-    baseUrl: "http://10.10.16.46:5002",
+    baseUrl: "http://10.10.1.99:5002",
   },
   qrPi: {
     deviceId: "pi-zero",
-    baseUrl: "http://10.10.28.38:5001",
+    baseUrl: "http://10.10.1.100:5001",
   },
 };
